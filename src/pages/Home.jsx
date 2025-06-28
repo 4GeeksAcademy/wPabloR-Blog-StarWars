acd91bf536418032021a32e17be09afd084ea33c
wPabloR <ricotep0@gmail.com>
2025-06-28 12:21:47 +0000

    main page created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,37 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
+
+	useEffect(() => {
+		fetch(`https://www.swapi.tech/api/people/`)
+			.then(resp => resp.json())
+			.then(data => {
+				dispatch({
+					type: 'load_character',
+					payload: data.results
+				})
+			})
+	}, [])
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="d-flex flex-wrap justify-content-center gap-3">
+			{store.people?.length > 0 && 
+			store.people.map(character => (
+				<div className="card" style={{ width: "18rem" }} key={character.uid}>
+					<img src="https://picsum.photos/300/200" className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">{character.name}</h5>
+						<p>{character.uid}</p>
+						<Link to={`/single/${character.uid}`} className="btn btn-primary">Learn more</Link>
+
+					</div>
+				</div>
+			))}
 		</div>
 	);
 }; 
--- src/store.js
+++ src/store.js
@@ -1,31 +1,19 @@
 export const initialStore=()=>{
   return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
+    people:[]
   }
 }
 
 export default function storeReducer(store, action = {}) {
   switch(action.type){
-    case 'add_task':
 
-      const { id,  color } = action.payload
+      case 'load_character':
 
-      return {
+      return{
         ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
+        people: action.payload
+      }
+      
     default:
       throw Error('Unknown action.');
   }    
